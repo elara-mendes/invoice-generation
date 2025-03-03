13e211e9145a238873faf44565e20e74f2e6f6a9
Elara <elaramendes@yahoo.com>
2025-03-03 16:18:29 -0300

    Added Header and rows to table

--- main.py
+++ main.py
@@ -17,7 +17,32 @@ for file in filepaths:
     pdf.set_font(style="B", family="Times", size=16)
     pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
     pdf.ln(10)
-    pdf.cell(w=50, h=8, txt=f"Date: {date_invoice}")
+    pdf.cell(w=50, h=8, txt=f"Date: {date_invoice}", ln=1)
+
+    pdf.ln(15)
+
+    # Header
+    columns_items = list(read_file.columns)
+    columns = [str(column).replace("_", " ").title() for column in columns_items]
+
+    pdf.set_font(family="Times", size=9, style="B")
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=45, h=8, txt=columns[1], border=1)
+    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1, ln=1)
+
+    # print(str(columns).replace("_", " ").title())
+
+    # Rows
+    for index, row in read_file.iterrows():
+        pdf.set_font(family="Times", size=9)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=45, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
     pdf.output(f"pdfs/{filename}.pdf")
     # print(date_invoice)
 
